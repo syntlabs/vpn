d460f610b5eb712e38327f4641aebf9e364a3af6
goldreborn <154214741+goldreborn@users.noreply.github.com>
2024-05-23 22:45:40 +0300

    commented everything

--- Client/ios/first_launch.swift
+++ Client/ios/first_launch.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+// Struct to hold the data needed for each ViewController
 struct ViewControllerData {
     let descriptionText: String
     let backgroundColor: UIColor
@@ -7,45 +8,72 @@ struct ViewControllerData {
     let buttonAction: () -> Void
 }
 
+// AbstractViewController is a subclass of UIViewController and serves as a base class for other view controllers
 class AbstractViewController: UIViewController {
+    // IBOutlet for the UILabel that displays the description text
     @IBOutlet var descriptionLabel: UILabel!
+    // IBOutlet for the UIButton that triggers the button action
     @IBOutlet var button: UIButton!
+    // IBOutlet for the UIView that creates the parallax effect
     @IBOutlet var parallaxView: UIView!
 
+    // IBInspectable property for the description text
     @IBInspectable var descriptionText: String?
+    // IBInspectable property for the background color
     @IBInspectable var backgroundColor: UIColor?
+    // IBInspectable property for the button title
     @IBInspectable var buttonTitle: String?
+    // IBInspectable property for the button action
     @IBInspectable var buttonAction: String?
+    // Property to keep track of the current index
     @IBInspectable var currentIndex: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Call the setupUI function to configure the user interface
         setupUI()
+        // Call the updateUI function to update the user interface for the current index
         updateUI(forIndex: currentIndex)
     }
 
+    // Function to setup the user interface
     func setupUI() {
+        // Set the background color of the view to white
         view.backgroundColor = UIColor.white
+        // Set the background color of the parallaxView to the background color property
         parallaxView.backgroundColor = backgroundColor
+        // Set the text of the descriptionLabel to the descriptionText property
         descriptionLabel.text = descriptionText
+        // Set the title of the button to the buttonTitle property
         button.setTitle(buttonTitle, for: .normal)
+        // Add a target to the button that triggers the buttonTapped function when the button is tapped
         button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
     }
 
+    // Function to update the user interface for the specified index
     func updateUI(forIndex index: Int) {
+        // Set the current index to the specified index
         currentIndex = index
+        // Get the ViewControllerData for the current index
         let viewControllerData = getViewControllerData(forIndex: index)
+        // Set the descriptionText property to the descriptionText of the ViewControllerData
         descriptionText = viewControllerData.descriptionText
+        // Set the backgroundColor property to the backgroundColor of the ViewControllerData
         backgroundColor = viewControllerData.backgroundColor
+        // Set the buttonTitle property to the buttonTitle of the ViewControllerData
         buttonTitle = viewControllerData.buttonTitle
+        // If the buttonAction property is not nil, remove the current target from the button and add a new target that triggers the specified action when the button is tapped
         if let actionString = buttonAction, let action = NSSelectorFromString(actionString) {
             button.removeTarget(nil, action: nil, for: .allEvents)
             button.addTarget(self, action: action, for: .touchUpInside)
         }
+        // Call the setupUI function to configure the user interface with the updated properties
         setupUI()
     }
 
+    // Function to get the ViewControllerData for the specified index
     func getViewControllerData(forIndex index: Int) -> ViewControllerData {
+        // Array of ViewControllerData objects
         let viewControllerDataArray = [
             ViewControllerData(
                 descriptionText: "This is View Controller 1",
@@ -72,17 +100,23 @@ class AbstractViewController: UIViewController {
                 buttonAction: { [weak self] in self?.skipToMainViewController() }
             )
         ]
+        // Return the ViewControllerData object at the specified index
         return viewControllerDataArray[index]
     }
 
+    // Function that is triggered when the button is tapped
     @objc func buttonTapped() {
+        // If the buttonAction property is not nil, perform the specified action
         if let action = buttonAction, let selector = NSSelectorFromString(action) {
             perform(selector)
         }
     }
 
+    // Function that is triggered when the "Next" button is tapped
     func nextButtonTapped() {
+        // Calculate the index of the next ViewController
         let nextIndex = currentIndex + 1
+        // If the next index is less than 4, create a new instance of AbstractViewController, update its user interface for the next index, and push it onto the navigation stack. Animate the transition to the new ViewController.
         if nextIndex < 4 {
             let nextViewController = AbstractViewController()
             nextViewController.updateUI(forIndex: nextIndex)
@@ -91,27 +125,34 @@ class AbstractViewController: UIViewController {
         }
     }
 
+    // Function to skip to the MainViewController
     func skipToMainViewController() {
-        let mainViewController = MainViewController() 
+        // Create a new instance of MainViewController and pop to it on the navigation stack
+        let mainViewController = MainViewController()
         navigationController?.popToViewController(mainViewController, animated: true)
     }
 
+    // Function to animate the transition to the specified ViewController
     func animateTransition(to viewController: UIViewController?) {
+        // If the viewController is nil, return from the function
         guard let viewController = viewController else { return }
+        // Get the width and height of the screen
         let screenWidth = UIScreen.main.bounds.width
         let screenHeight = UIScreen.main.bounds.height
-
+        // Get the current view and the view of the specified ViewController
         let fromView = view
         let toView = viewController.view
-
+        // Calculate the x and y offsets for the parallax effect
         let xOffset = view.bounds.width * 0.2
         let yOffset = view.bounds.height * 0.2
+        // Set the duration of the animation
         let animationDuration = 0.3
-
+        // Animate the transition using a cross dissolve effect. Move the current view and the parallaxView of the specified ViewController to their final positions.
         UIView.transition(with: fromView, duration: animationDuration, options: .transitionCrossDissolve, animations: {
             fromView.transform = CGAffineTransform(translationX: -xOffset, y: -yOffset)
             toView.transform = CGAffineTransform.identity
         }, completion: { _ in
+            // If the specified ViewController is an instance of AbstractViewController, animate the parallaxView to its final position
             if let viewController = viewController as? AbstractViewController {
                 viewController.parallaxView.transform = CGAffineTransform(translationX: -xOffset, y: -yOffset)
                 UIView.animate(withDuration: animationDuration, delay: 0, options: .curveEaseInOut, animations: {
--- Client/ios/vpn_manager.swift
+++ Client/ios/vpn_manager.swift
@@ -1,48 +1,136 @@
 import CFNetwork
 import NetworkExtension
 
+/**
+ * VPNManager class is responsible for managing the VPN connection.
+ * It provides methods to start and stop the VPN tunnel.
+ */
 class VPNManager {
+    /**
+     * Shared instance of NEVPNManager, which is used to manage the VPN configuration.
+     */
     let vpnManager = NEVPNManager.shared()
+    
+    /**
+     * Data representation of the password, stored as a reference to be used for authentication.
+     */
     let passwordRef: Data
-
+    
+    /**
+     * Initializes the VPNManager instance with a password.
+     * The password is converted to Data using UTF-8 encoding and stored as a reference.
+     *
+     * - parameter password: The password to be used for authentication.
+     */
     init(password: String) {
-        passwordRef = password.data(using: .utf8)!
+        passwordRef = password.data(using:.utf8)!
     }
-
+    
+    /**
+     * Starts the VPN tunnel by loading the VPN configuration from preferences,
+     * setting up the protocol configuration, and saving the changes to preferences.
+     */
     func startVPN() {
+        /**
+         * Creates a new instance of NEVPNProtocol, which represents the VPN protocol configuration.
+         */
         let vpnProtocol = NEVPNProtocol()
+        
+        /**
+         * Sets the provider bundle identifier, which is used to identify the VPN provider.
+         */
         vpnProtocol.providerBundleIdentifier = "com.example.vpn"
+        
+        /**
+         * Sets the server address, which is the URL of the VPN server.
+         */
         vpnProtocol.serverAddress = "your-proxy-server-url.com"
+        
+        /**
+         * Sets the username, which is used for authentication.
+         */
         vpnProtocol.username = "username"
+        
+        /**
+         * Sets the password reference, which is used for authentication.
+         */
         vpnProtocol.passwordReference = passwordRef
-        vpnProtocol.authenticationMethod = .none
+        
+        /**
+         * Sets the authentication method to none, which means no additional authentication is required.
+         */
+        vpnProtocol.authenticationMethod =.none
+        
+        /**
+         * Enables extended authentication, which allows for more advanced authentication mechanisms.
+         */
         vpnProtocol.useExtendedAuthentication = true
-
+        
+        /**
+         * Creates a new instance of NEVPNCrypto, which represents the encryption and integrity algorithms.
+         */
         let chacha20Encryption = NEVPNCrypto()
-        chacha20Encryption.encryptionAlgorithm = .chacha20
-        chacha20Encryption.integrityAlgorithm = .poly1305
+        
+        /**
+         * Sets the encryption algorithm to ChaCha20, which is a fast and secure encryption algorithm.
+         */
+        chacha20Encryption.encryptionAlgorithm =.chacha20
+        
+        /**
+         * Sets the integrity algorithm to Poly1305, which is a fast and secure integrity algorithm.
+         */
+        chacha20Encryption.integrityAlgorithm =.poly1305
+        
+        /**
+         * Sets the crypto configuration to the VPN protocol.
+         */
         vpnProtocol.crypto = chacha20Encryption
-
+        
+        /**
+         * Loads the VPN configuration from preferences, which may contain previously saved settings.
+         * If an error occurs, it is printed to the console.
+         */
         vpnManager.loadFromPreferences { error in
             if let error = error {
                 print("Error loading VPN preferences: \(error)")
                 return
             }
-
+            
+            /**
+             * Sets the protocol configuration to the VPN manager.
+             */
             self.vpnManager.protocolConfiguration = vpnProtocol
+            
+            /**
+             * Enables on-demand VPN, which allows the VPN to start automatically when needed.
+             */
             self.vpnManager.isOnDemandEnabled = true
+            
+            /**
+             * Saves the VPN configuration to preferences, which persists the changes.
+             * If an error occurs, it is printed to the console.
+             */
             self.vpnManager.saveToPreferences { error in
                 if let error = error {
                     print("Error saving VPN preferences: \(error)")
                     return
                 }
-
+                
+                /**
+                 * Starts the VPN tunnel, which establishes the connection to the VPN server.
+                 */
                 self.vpnManager.startVPNTunnel()
             }
         }
     }
-
+    
+    /**
+     * Stops the VPN tunnel, which disconnects the VPN connection.
+     */
     func stopVPN() {
+        /**
+         * Stops the VPN tunnel, which disconnects the VPN connection.
+         */
         vpnManager.stopVPNTunnel()
     }
 }
